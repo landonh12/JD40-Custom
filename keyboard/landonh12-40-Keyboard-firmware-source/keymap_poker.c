ffb6b70297d92f0587b52d86ac88da31a75f38ef
Landon Haugh <landon.haugh1@gmail.com>
2016-05-22 03:30:12 -0500

    Initial commit

---
+++ keyboard/landonh12-40-Keyboard-firmware-source/keymap_poker.c
@@ -0,0 +1,37 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: qwerty */
+    KEYMAP(
+        ESC, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, BSPC, \
+        TAB, A,   S,   D,   F,   G,   H,   J,   K,   L,   ENT,  \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,  DOT, FN1, FN0, \
+        LCTL,LGUI,LALT,          SPC,                     FN2, PAUS),
+
+    /* 1: FN 0 */
+    KEYMAP(
+        TRNS, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   EQL, \
+        TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  SCLN,  QUOT,  BSLS,  \
+        TRNS, UP, F11, F12, LBRC ,RBRC ,QUOT,SCLN,COMM,SLSH,FN0, \
+        LEFT, DOWN,RGHT,          TRNS,               MINS,RCTL),
+    
+    /* 2: FN 1 */
+    KEYMAP(
+           ESC, 1,   W,   2,   3,   4,   5,   6,   7,   8,   9, BSPC, \
+           TAB, A,   S,   D,   F,   G,   H,   J,   K,   L,   ENT,  \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,  DOT, FN1, RSFT, \
+           LCTL,LGUI,LALT,          SPC,                     COMM, SLSH),
+    
+    /* 3: FN 2 */
+    KEYMAP(
+           ESC, 1,   W,   2,   R,   4,   5,   6,   7,   8,   9, BSPC, \
+           TAB, A,   S,   D,   3,   G,   H,   J,   K,   L,   ENT,  \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,  DOT, RSFT, PPLS, \
+           LCTL,LGUI,LALT,          SPC,                     FN2, PAUS),
+
+};
+const uint16_t PROGMEM fn_actions[] = {
+    [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_LAYER_TOGGLE(2),
+    [2] = ACTION_LAYER_TOGGLE(3),
+};
