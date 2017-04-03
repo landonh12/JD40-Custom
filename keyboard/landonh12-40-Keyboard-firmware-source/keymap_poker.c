09e626ca0e35d19fcb8b428fe45b40d3d0907ce7
Landon Haugh <Landon@Landons-MacBook-Pro.local>
2017-04-03 12:13:24 -0500

    Removed some layers and changed the function layer keymap.

--- keyboard/landonh12-40-Keyboard-firmware-source/keymap_poker.c
+++ keyboard/landonh12-40-Keyboard-firmware-source/keymap_poker.c
@@ -10,25 +10,25 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* 1: FN 0 */
     KEYMAP(
-        TRNS, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   EQL, \
-        TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  SCLN,  QUOT,  BSLS,  \
-        TRNS, UP, F11, F12, LBRC ,RBRC ,QUOT,SCLN,COMM,SLSH,FN0, \
-        LEFT, DOWN,RGHT,          TRNS,               MINS,RCTL),
+        TRNS, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,TRNS, \
+        TRNS, F1,  F2,  F3,  F4,  F5,  F6, EQL, SCLN,QUOT,   BSLS,  \
+        TRNS, UP, F11, F12, LBRC,RBRC,QUOT,SCLN,COMM,SLSH,   FN0, \
+        LEFT, DOWN,RGHT,          TRNS,               MINS,  RCTL),
     
-    /* 2: FN 1 */
+    /* 2: FN 1 
     KEYMAP(
            ESC, 1,   W,   2,   3,   4,   5,   6,   7,   8,   9, BSPC, \
            TAB, A,   S,   D,   F,   G,   H,   J,   K,   L,   ENT,  \
            LSFT,Z,   X,   C,   V,   B,   N,   M,  DOT, FN1, RSFT, \
            LCTL,LGUI,LALT,          SPC,                     COMM, SLSH),
     
-    /* 3: FN 2 */
+    /* 3: FN 2 
     KEYMAP(
            ESC, 1,   W,   2,   R,   4,   5,   6,   7,   8,   9, BSPC, \
            TAB, A,   S,   D,   3,   G,   H,   J,   K,   L,   ENT,  \
            LSFT,Z,   X,   C,   V,   B,   N,   M,  DOT, RSFT, PPLS, \
            LCTL,LGUI,LALT,          SPC,                     FN2, PAUS),
-
+    */
 };
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
