a905a7d179e95a4807d07590f54a0eddb867fd9d
Landon Haugh <Landon@Landons-MacBook-Pro.local>
2017-04-03 17:08:53 -0500

    Fixed weird key issue

--- keyboard/landonh12-40-Keyboard-firmware-source/keymap_poker.c
+++ keyboard/landonh12-40-Keyboard-firmware-source/keymap_poker.c
@@ -6,14 +6,14 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ESC, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, BSPC, \
         TAB, A,   S,   D,   F,   G,   H,   J,   K,   L,   ENT,  \
         LSFT,Z,   X,   C,   V,   B,   N,   M,  DOT, COMM, FN0, \
-        LCTL,LGUI,LALT,          SPC,                     FN2, PAUS),
+        LCTL,LGUI,LALT,          SPC,                     RALT, RCTL),
 
     /* 1: FN 0 */
     KEYMAP(
         TRNS, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,TRNS, \
         TRNS, F1,  F2,  F3,  F4,  F5,  F6, EQL, SCLN,QUOT,   BSLS,  \
         TRNS, UP, F11, F12, LBRC,RBRC,QUOT,SCLN,TRNS,SLSH,   FN0, \
-        LEFT, DOWN,RGHT,          TRNS,               MINS,  RCTL),
+        LEFT, DOWN,RGHT,          TRNS,               MINS,  TRNS),
     
     /* 2: FN 1 
     KEYMAP(
